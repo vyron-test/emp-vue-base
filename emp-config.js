a48e9f74a15d3ba711d0268e5e4d3ccb79275229
vyron <fiee_vyron@163.com>
2021-12-29 14:06:50 +0800

    feat: init emp base project

---
+++ emp-config.js
@@ -0,0 +1,32 @@
+const { defineConfig } = require('@efox/emp');
+const vue = require('@efox/plugin-vue-2');
+const path = require('path');
+
+module.exports = defineConfig({
+    plugins: [vue],
+    server: {
+        port: 8001
+    },
+    appEntry: "main.js",
+    empShare: {
+        name: 'microBase',
+        exposes: {
+            "./App": "./src/App.vue",
+            "./HelloWorld": "./src/components/HelloWorld.vue"
+        }
+    },
+    webpackChain(config) {
+        config.resolve.alias.set('@', path.resolve('/', 'src'));
+        config.resolve.alias.set('vue', 'vue/dist/vue.esm.js');
+        // config.module.rule("scss-loader").test(/\.scss$/).use("sass-loader").loader('sass-loader').options({
+        //     additionalData: `@import "./src/assets/css/variables/variables.scss";`
+        // });
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                // prependData: `@import "./src/assets/css/variables/variables.scss";`
+            }
+        }
+    }
+});
